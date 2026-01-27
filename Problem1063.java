5f017e4cd3abd62ac77b7d9928fa5b12bc6133ce
Leegeon <leegeondev@gmail.com>
2026-01-28 00:01:33 +0900

    fix: 백준 실버3 킹 문제 해결

--- Problem1063.java
+++ Problem1063.java
@@ -1,68 +1,64 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-/**
- * ▣ 문제: 백준: 1063 - 킹
- * ▣ 알고리즘:
- * ▣ 시간 복잡도:
- * * [풀이 전략]
- * 1.
- * 2.
- */
 public class Problem1063 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 0: y, 1: x
-        int[] kingPoint = getPoint(st.nextToken());
-        int[] dollPoint = getPoint(st.nextToken());
+        int[] king = getPoint(st.nextToken());
+        int[] stone = getPoint(st.nextToken());
         int moveCount = Integer.parseInt(st.nextToken());
 
-        String command;
-        int crtY, crtX;
-        int dollY, dollX;
-        for(int i = 0; i < moveCount; i++) {
-            command = br.readLine();
-            crtY = kingPoint[0];
-            crtX = kingPoint[1];
-            dollY = dollPoint[0];
-            dollX = dollPoint[1];
-            switch(command) {
-                case "R" : crtX++; dollX++; break;
-                case "L" : crtX--; dollX--; break;
-                case "B" : crtY++; dollY++; break;
-                case "T" : crtY--; dollY--; break;
-                case "RT" : crtX++; crtY--; dollX++; dollY--; break;
-                case "LT" : crtX--; crtY--; dollX--; dollY--;break;
-                case "RB" : crtX++; crtY++; dollX++; dollY++;break;
-                case "LB" : crtX--; crtY++; dollX--; dollY++;break;
+        for (int i = 0; i < moveCount; i++) {
+            String command = br.readLine();
+            int dx = 0, dy = 0;
+
+            switch (command) {
+                case "R":  dx = 1;  break;
+                case "L":  dx = -1; break;
+                case "B":  dy = -1; break;
+                case "T":  dy = 1;  break;
+                case "RT": dx = 1;  dy = 1;  break;
+                case "LT": dx = -1; dy = 1;  break;
+                case "RB": dx = 1;  dy = -1; break;
+                case "LB": dx = -1; dy = -1; break;
             }
-            if(isOver(crtY, crtX)) continue;
-            if(dollPoint[0] == crtY && dollPoint[1] == crtX) {
-                if(isOver(crtY, crtX)) continue;
-                dollPoint[0] = dollY;
-                dollPoint[1] = dollX;
+
+            int nkx = king[0] + dx;
+            int nky = king[1] + dy;
+
+            if (isOver(nkx, nky)) continue;
+
+            if (nkx == stone[0] && nky == stone[1]) {
+                int nsx = stone[0] + dx;
+                int nsy = stone[1] + dy;
+
+                if (isOver(nsx, nsy)) continue;
+
+                stone[0] = nsx;
+                stone[1] = nsy;
             }
-            kingPoint[0] = crtY;
-            kingPoint[1] = crtX;
+
+            king[0] = nkx;
+            king[1] = nky;
         }
-        bw.write((char)('A'+kingPoint[1])+""+kingPoint[0] + "\n");
-        bw.write((char)('A'+dollPoint[1])+""+dollPoint[0] + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+
+        System.out.println(makePos(king));
+        System.out.println(makePos(stone));
     }
 
     private static int[] getPoint(String str) {
         int x = str.charAt(0) - 'A';
         int y = str.charAt(1) - '1';
-        return new int[]{y, x};
+        return new int[]{x, y};
     }
 
-    private static boolean isOver(int y, int x) {
-        if(y < 0 || x < 0) return true;
-        return y >= 8 || x >= 8;
+    private static boolean isOver(int x, int y) {
+        return x < 0 || x >= 8 || y < 0 || y >= 8;
+    }
+
+    private static String makePos(int[] point) {
+        return (char) (point[0] + 'A') + "" + (point[1] + 1);
     }
 }
